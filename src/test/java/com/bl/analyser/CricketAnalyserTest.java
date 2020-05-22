eb73e76539eab9046f1d860e7c0e7a9105e2a70c
HP <varadesharad@gmail.com>
2020-05-22 17:17:54 +0530

    Added:Test to get top striking rate of the Cricketers who played IPL

--- src/test/java/com/bl/analyser/CricketAnalyserTest.java
+++ src/test/java/com/bl/analyser/CricketAnalyserTest.java
@@ -1,4 +1,28 @@
 package com.bl.analyser;
 
+import com.bl.model.BatsmanCSVDAO;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.List;
+
 public class CricketAnalyserTest {
+
+    private static final String IPL_MOST_RUN_CSV_FILE_PATH = "src/test/resources/IPL2019FactsheetMostRuns.csv";
+
+    @Test
+    public void givenIPLCricketLeagueData_WhenSortedOnAvg_ShouldReturnTopBattingAveragePlayer() {
+        CricketAnalyser cricketAnalyser = new CricketAnalyser(CricketAnalyser.Role.BATTING);
+        cricketAnalyser.loadIPLCricketerData(IPL_MOST_RUN_CSV_FILE_PATH);
+        List<BatsmanCSVDAO> topBattingAverage = cricketAnalyser.getTopStatus("Avg");
+        Assert.assertEquals("MS Dhoni",topBattingAverage.get(0).player);
+    }
+
+    @Test
+    public void givenIPLCricketerLeagueData_WhenSortedOnAvg_ShouldReturnTopStrikingRate() {
+        CricketAnalyser cricketAnalyser = new CricketAnalyser(CricketAnalyser.Role.BATTING);
+        cricketAnalyser.loadIPLCricketerData(IPL_MOST_RUN_CSV_FILE_PATH);
+        List<BatsmanCSVDAO> topSrikingRate = cricketAnalyser.getTopStatus("StrikeRate");
+        Assert.assertEquals("Ishant Sharma",topSrikingRate.get(0).player);
+    }
 }
